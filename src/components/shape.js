08aa7764fdb40137ef0bcd0231a3dacca86a07d3
RamNarayan <108714679+Sameint@users.noreply.github.com>
2023-01-06 00:38:20 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,19 @@
 // declare class
-
-// export class using module.exports
+class Shape{
+  constructor(color) {
+    if (color) {
+        this.color = color;
+    } else {
+        this.color = 'red';
+    }
+  }
+  
+  drawShape(){
+  
+  }
+  calculateArea(){
+  
+  }
+  
+}
+module.exports = Shape;
